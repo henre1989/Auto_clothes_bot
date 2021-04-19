07cee9dd17aeb8786c61433ef9fecd5ea06e08e1
henre1989 <transfer1989@gmail.com>
2021-04-19 15:07:33 +0500

    add def send_code_clothes

--- bot.py
+++ bot.py
@@ -233,6 +233,38 @@ def send_welcome(message):
         logging.info(str(chat_id) + ' send_welcome ' + str(e))
 
 
+@bot.message_handler(commands=['sendcode'])
+def send_code_clothes(message):
+    f.open(PATH + 'code.txt', 'r', encoding='utf-8')
+    main_chat = f.readlines()
+    chat_id = message.chat.id
+    if chat_id in main_chat:
+        logging.info(str(chat_id) + ' отправитель команды /sendcode')
+        sql = "SELECT * FROM clothes"
+        alll = sql_requests(sql)
+        for str_chat_id in alll:
+            chat_id = str(str_chat_id[0])
+            if chat_id == 'None':
+                continue
+            elif chat_id == '':
+                continue
+            try:
+                locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
+                today = datetime.today().strftime("%d %b")
+                bot.send_message(chat_id,
+                                 'Для того, что бы понимать в каком состоянии у вас корпоративная одежда и '
+                                 'одели ли вы ее в принципе, 5-6 раз в год будет приходить сообщение в Telegram с '
+                                 'произвольным кодовым словом. Вам, в течение часа, необходимо снять видео и '
+                                 'произнести данный код. \nКодовое слово на сегодня: ' + str(today))
+                logging.info(str(chat_id) + ' оповещен')
+                sleep(1)
+            except Exception as e:
+                logging.info(e)
+                logging.info(str(chat_id) + ' заблокировал бота или нет такого пользователя')
+
+
+
+
 def reg_new_employees(message):
     try:
         chat_id = message.chat.id
